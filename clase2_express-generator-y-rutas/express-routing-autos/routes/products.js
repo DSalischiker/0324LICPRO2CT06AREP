1d7b67fd0d38f762b6b9e56a6e8e093f21013c32
DSalischiker <diego.salischiker@studiohakuna.com>
2024-03-07 17:37:23 -0300

    Ejercitación express-generator y rutas en clase del 4/3

---
+++ clase2_express-generator-y-rutas/express-routing-autos/routes/products.js
@@ -0,0 +1,30 @@
+let express = require('express');
+let router = express.Router();
+let autos = require('../db/index.js');
+
+router.get('/', function(req, res) {
+  return res.send(autos.lista);
+});
+
+router.get('/color/:color', function(req, res) {
+  let color = req.params.color;
+  let resultadoBusqueda = [];
+
+  for (let i = 0; i < autos.lista.length; i++) {
+    if (autos.lista[i].color === color.toLowerCase()) {
+      resultadoBusqueda.push(autos.lista[i]);
+    }
+  }
+
+  if (resultadoBusqueda.length > 0) {
+    return res.send(resultadoBusqueda);
+  } else {
+    return res.send(`No hay autos de color ${color}.`);
+  }
+});
+
+// Ruta para traer autos por marca
+
+// Ruta para traer autos por modelo (y año)
+
+module.exports = router;
